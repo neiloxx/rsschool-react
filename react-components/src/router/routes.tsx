e86e38ff44f8d82abc0404041133e9b635db0057
Anastasia Sidorenko <anastasia.v.sido@gmail.com>
2023-03-19 14:11:50 +0300

    feat: add notFoundPage & redirect

--- react-components/src/router/routes.tsx
+++ react-components/src/router/routes.tsx
@@ -1,5 +1,6 @@
 import BaseLayout from 'components/Layouts/BaseLayout';
 import pages from 'pages/index';
+import { Navigate } from 'react-router-dom';
 
 export default [
   {
@@ -11,9 +12,17 @@ export default [
         element: <pages.HomePage />,
       },
       {
-        path: '/about',
+        path: 'about',
         element: <pages.AboutPage />,
       },
     ],
+  },
+  {
+    path: '/404',
+    element: <pages.NotFoundPage />,
+  },
+  {
+    path: '*',
+    element: <Navigate to={'/404'} />,
   },
 ];
