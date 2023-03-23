daf4ffd11f37af8fbba8aef9430c2aebbcdb9d3e
Anastasia Sidorenko <anastasia.v.sido@gmail.com>
2023-03-23 19:03:12 +0300

    refactor: move responsibility from CardsField to its parents

--- src/components/CardsField/CardsField.tsx
+++ src/components/CardsField/CardsField.tsx
@@ -1,14 +1,21 @@
-import Card from 'components/Card/Card';
 import React from 'react';
-import data from 'api/data.json';
+
+import Card from 'components/Card/Card';
+import { CardType } from 'types/types';
 
 import 'components/CardsField/CardsField.scss';
 
-export default class CardsField extends React.Component {
+type CardsFieldType = {
+  cards: CardType[];
+};
+
+export default class CardsField extends React.Component<CardsFieldType> {
+  data = this.props.cards;
+
   render() {
     return (
       <div className={'cards'}>
-        {data.map((el, idx) => (
+        {this.data.map((el, idx) => (
           <Card
             key={`${el.title}-${idx}`}
             title={el.title}
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,13 +2,15 @@ import CardsField from 'components/CardsField/CardsField';
 import SearchBar from 'components/SearchBar/SearchBar';
 import React from 'react';
 
+import data from 'api/data.json';
+
 export default class HomePage extends React.Component {
   render() {
     return (
       <>
         <h1>Home Page</h1>
         <SearchBar />
-        <CardsField />
+        <CardsField cards={data} />
       </>
     );
   }
