c0a2fba7687e76687201732c770e6ad29d02e2d2
Anastasia Sidorenko <anastasia.v.sido@gmail.com>
2023-04-02 15:46:39 +0300

    refactor: migrate pages to hooks

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,17 +1,14 @@
+import data from 'api/data.json';
 import CardsField from 'components/CardsField/CardsField';
 import SearchBar from 'components/SearchBar/SearchBar';
 import React from 'react';
 
-import data from 'api/data.json';
-
-export default class HomePage extends React.Component {
-  render() {
-    return (
-      <>
-        <h1>Home Page</h1>
-        <SearchBar />
-        <CardsField cards={data} />
-      </>
-    );
-  }
+export default function HomePage(): JSX.Element {
+  return (
+    <>
+      <h1>Home Page</h1>
+      <SearchBar />
+      <CardsField cards={data} />
+    </>
+  );
 }
--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,18 +1,15 @@
-import React from 'react';
-
 import 'pages/NotFoundPage/NotFoundPage.scss';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-export default class NotFoundPage extends React.Component {
-  render() {
-    return (
-      <main className={'main-nfp'}>
-        <h2 className={'main-nfp__title'}>Oops, page not found</h2>
-        <div className={'main-nfp__image'} />
-        <NavLink className={'main-nfp__link'} to={'/'}>
-          Go home
-        </NavLink>
-      </main>
-    );
-  }
+export default function NotFoundPage(): JSX.Element {
+  return (
+    <main className={'main-nfp'}>
+      <h2 className={'main-nfp__title'}>Oops, page not found</h2>
+      <div className={'main-nfp__image'} />
+      <NavLink className={'main-nfp__link'} to={'/'}>
+        Go home
+      </NavLink>
+    </main>
+  );
 }
