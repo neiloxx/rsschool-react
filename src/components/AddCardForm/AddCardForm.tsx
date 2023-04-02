a64cc71545f0aa62d18b4d93a9ca8cae8e356c29
Anastasia Sidorenko <anastasia.v.sido@gmail.com>
2023-04-02 16:01:08 +0300

    refactor: migrate add card form component to hooks

--- src/components/AddCardForm/AddCardForm.tsx
+++ src/components/AddCardForm/AddCardForm.tsx
@@ -2,7 +2,7 @@ import Popup from 'components/Popup/Popup';
 import Button from 'components/ui/buttons/Button';
 import Dropdown from 'components/ui/dropdown/Dropdown';
 import FileInput from 'components/ui/inputs/FileInput';
-import React from 'react';
+import React, { useState } from 'react';
 import generateId from 'utils/generateId';
 
 import Form from 'components/Form/Form';
@@ -27,131 +27,122 @@ type AddCardFormProps = {
   addCard: (card: CardType) => void;
 };
 
-type FormState = {
-  errors: { [x: string]: string[] };
-  isPopupOpened: boolean;
-};
+export default function AddCardForm({ addCard }: AddCardFormProps) {
+  const [errors, setErrors] = useState<{ [x: string]: string[] }>({});
+  const [isPopupOpened, setPopupOpened] = useState<boolean>(false);
 
-export default class AddCardForm extends React.Component<AddCardFormProps> {
-  form: React.RefObject<HTMLFormElement> = React.createRef();
-  validators = {
+  const form: React.RefObject<HTMLFormElement> = React.createRef();
+  const validators = {
     authors: () => validateAuthors(fields.authors.refProp.current!.value),
     title: () => validateTitle(fields.title.refProp.current!.value),
     date: () =>
       validatePublishDate(
         `${fields.date.refProp.current!.value}`,
-        this.getCheckedValue(fields.switch.refProps).join('')
+        getCheckedValue(fields.switch.refProps).join('')
       ),
-    categories: () => validateCategories(this.getCheckedValue(fields.checkbox.refProps)),
+    categories: () => validateCategories(getCheckedValue(fields.checkbox.refProps)),
     thumbnailUrl: () => validateImage(fields.thumbnailUrl.refProp.current?.files),
   };
 
-  state: FormState = {
-    errors: {},
-    isPopupOpened: false,
-  };
-
-  getCheckedValue = (refProps: React.RefObject<HTMLInputElement>[]): string[] => {
+  const getCheckedValue = (refProps: React.RefObject<HTMLInputElement>[]): string[] => {
     const checkedRefs = refProps.filter((el) => el.current?.checked);
     return checkedRefs.map((el) => el.current!.value);
   };
 
-  getImageUrl = () => {
+  const getImageUrl = () => {
     const files = fields.thumbnailUrl.refProp.current?.files;
     const file = files && files[0];
     return file ? URL.createObjectURL(file) : undefined;
   };
 
-  getValues = (): CardType => {
+  const getValues = (): CardType => {
     return {
       authors: fields.authors.refProp.current!.value,
-      categories: this.getCheckedValue(fields.checkbox.refProps),
+      categories: getCheckedValue(fields.checkbox.refProps),
       id: generateId(),
       language: fields.dropdown.refProp.current!.value,
       publishedDate: fields.date.refProp.current?.value,
-      status: this.getCheckedValue(fields.switch.refProps).join(''),
-      thumbnailUrl: this.getImageUrl(),
+      status: getCheckedValue(fields.switch.refProps).join(''),
+      thumbnailUrl: getImageUrl(),
       title: fields.title.refProp.current!.value,
     };
   };
 
-  onFormError = (errors: FormErrorsType) => {
-    this.setState({ errors: { ...errors } });
+  const onFormError = (errors: FormErrorsType) => {
+    setErrors({ ...errors });
   };
 
-  onFormSuccess = () => {
-    this.props.addCard(this.getValues());
-    this.setState({ errors: {} });
-    this.setState({ isPopupOpened: true });
+  const onFormSuccess = () => {
+    addCard(getValues());
+    setErrors({});
+    setPopupOpened(true);
   };
 
-  render() {
-    return (
-      <>
-        <Form
-          refProp={this.form}
-          onFormError={this.onFormError}
-          onFormSuccess={this.onFormSuccess}
-          validators={this.validators}
-        >
-          <TextInput
-            id={fields.title.id}
-            label={fields.title.label}
-            refProp={fields.title.refProp}
-            errors={this.state.errors.title}
+  return (
+    <>
+      <Form
+        refProp={form}
+        onFormError={onFormError}
+        onFormSuccess={onFormSuccess}
+        validators={validators}
+      >
+        <TextInput
+          id={fields.title.id}
+          label={fields.title.label}
+          refProp={fields.title.refProp}
+          errors={errors.title}
+        />
+        <TextInput
+          id={fields.authors.id}
+          label={fields.authors.label}
+          refProp={fields.authors.refProp}
+          errors={errors.authors}
+        />
+        <Switch
+          id={fields.switch.id}
+          labels={fields.switch.labels}
+          refProps={fields.switch.refProps}
+        />
+        <DateInput
+          id={fields.date.id}
+          label={fields.date.label}
+          refProp={fields.date.refProp}
+          errors={errors.date}
+        />
+        <CheckboxField
+          id={fields.checkbox.id}
+          labels={fields.checkbox.labels}
+          refProps={fields.checkbox.refProps}
+          errors={errors.categories}
+        />
+        <Dropdown
+          id={fields.dropdown.id}
+          label={fields.dropdown.label}
+          options={fields.dropdown.options}
+          refProp={fields.dropdown.refProp}
+        />
+        <FileInput
+          id={fields.thumbnailUrl.id}
+          label={fields.thumbnailUrl.label}
+          formats={fields.thumbnailUrl.formats}
+          refProp={fields.thumbnailUrl.refProp}
+          errors={errors.thumbnailUrl}
+        />
+        <Button type="submit" innerText="Add card" />
+      </Form>
+      {isPopupOpened && (
+        <Popup>
+          <p>Your data has been saved!</p>
+          <Button
+            type="button"
+            innerText="good"
+            onClick={() => {
+              setPopupOpened(false);
+              form.current?.reset();
+            }}
           />
-          <TextInput
-            id={fields.authors.id}
-            label={fields.authors.label}
-            refProp={fields.authors.refProp}
-            errors={this.state.errors.authors}
-          />
-          <Switch
-            id={fields.switch.id}
-            labels={fields.switch.labels}
-            refProps={fields.switch.refProps}
-          />
-          <DateInput
-            id={fields.date.id}
-            label={fields.date.label}
-            refProp={fields.date.refProp}
-            errors={this.state.errors.date}
-          />
-          <CheckboxField
-            id={fields.checkbox.id}
-            labels={fields.checkbox.labels}
-            refProps={fields.checkbox.refProps}
-            errors={this.state.errors.categories}
-          />
-          <Dropdown
-            id={fields.dropdown.id}
-            label={fields.dropdown.label}
-            options={fields.dropdown.options}
-            refProp={fields.dropdown.refProp}
-          />
-          <FileInput
-            id={fields.thumbnailUrl.id}
-            label={fields.thumbnailUrl.label}
-            formats={fields.thumbnailUrl.formats}
-            refProp={fields.thumbnailUrl.refProp}
-            errors={this.state.errors.thumbnailUrl}
-          />
-          <Button type="submit" innerText="Add card" />
-        </Form>
-        {this.state.isPopupOpened && (
-          <Popup>
-            <p>Your data has been saved!</p>
-            <Button
-              type="button"
-              innerText="good"
-              onClick={() => {
-                this.setState({ isPopupOpened: false });
-                this.form.current?.reset();
-              }}
-            />
-          </Popup>
-        )}
-      </>
-    );
-  }
+        </Popup>
+      )}
+    </>
+  );
 }
